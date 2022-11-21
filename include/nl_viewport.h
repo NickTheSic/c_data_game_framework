bed11f046a80e70f0ad907d4fb487984988c3081
Nick L <n_guitar_l@hotmail.com>
2022-11-20 23:27:51 -0500

    Removed the version of the function I probably won't use, moved it to the cpp file

--- include/nl_viewport.h
+++ include/nl_viewport.h
@@ -10,24 +10,6 @@ struct Viewport
     v2i screen_center;
 };
 
-v2f GetMouseInViewportWithCamera(Viewport* viewport, Camera *camera, v2i mouse_pos)
-{
-    const float camera_size_x = camera->size.x*2;
-    const float camera_size_y = camera->size.y*2;
-    
-    float fpos_x = ((static_cast<float>(mouse_pos.x) - camera_size_x) * 0.5f) + viewport->screen_size.x;
-    float fpos_y = ((static_cast<float>(mouse_pos.y) - camera_size_y) * 0.5f) + viewport->screen_size.y;
-
-    return {fpos_x, fpos_y};
-}
-
-void GetMouseInViewportWithCamera(Viewport* viewport, Camera* camera, v2i mouse_pos, v2f* out_pos)
-{
-    const float camera_size_x = camera->size.x*2;
-    const float camera_size_y = camera->size.y*2;
-
-    out_pos->x = ((static_cast<float>(mouse_pos.x) - camera_size_x) * 0.5f) + viewport->screen_size.x;
-    out_pos->y = ((static_cast<float>(mouse_pos.y) - camera_size_y) * 0.5f) + viewport->screen_size.y;
-}
+void GetMouseInViewportWithCamera(v2f* out_pos, Viewport* viewport, Camera* camera, v2i mouse_pos);
 
 #endif
--- src/nl_viewport.cpp
+++ src/nl_viewport.cpp
@@ -1,2 +1,10 @@
 #include <nl_viewport.h>
 
+void GetMouseInViewportWithCamera(v2f* out_pos, Viewport* viewport, Camera* camera, v2i mouse_pos)
+{
+    const float camera_size_x = camera->size.x*2;
+    const float camera_size_y = camera->size.y*2;
+
+    out_pos->x = ((static_cast<float>(mouse_pos.x) - camera_size_x) * 0.5f) + viewport->screen_size.x;
+    out_pos->y = ((static_cast<float>(mouse_pos.y) - camera_size_y) * 0.5f) + viewport->screen_size.y;
+}
