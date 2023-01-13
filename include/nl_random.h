50ff7b997c55a109e1264f250e487a39286336ad
Nick L <n_guitar_l@hotmail.com>
2023-01-12 23:15:21 -0500

    Moved the random function out of the math header

---
+++ include/nl_random.h
@@ -0,0 +1,19 @@
+
+#ifndef NL_RANDOM_H_
+#define NL_RANDOM_H_
+
+static unsigned int g_random_seed = 42069;
+static unsigned int 
+XORRandom(unsigned int seed = g_random_seed)
+{
+    g_random_seed = seed;
+
+    g_random_seed ^= g_random_seed << 13;
+    g_random_seed ^= g_random_seed >> 17;
+    g_random_seed ^= g_random_seed << 5;
+
+    return g_random_seed;
+}
+
+
+#endif NL_RANDOM_H_
