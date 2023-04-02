4be77c22a6d109c9246b2253f6fef66a205a6bfc
Nick L <n_guitar_l@hotmail.com>
2023-04-01 22:48:52 -0400

    Removede Whitespace

--- include/nl_game.h
+++ include/nl_game.h
@@ -5,13 +5,10 @@
 typedef struct GameData GameData;
 
 GameData* GameInitialize(struct Platform* platform);
-
 void GameUpdate(struct Platform* platform, GameData* data, float delta_time);
-
 // I might want to update this to be a platfrom / framework thing
 // I could allow the game custom drawing but I also want to just draw the active sprites at their position
 void GameRender(struct Platform* platform, GameData* data);
-
 void GameCleanup(GameData* data);
 
 #endif //_NL_GAME_H
